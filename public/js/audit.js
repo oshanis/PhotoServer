e0394f738be602fcdfc649ca0dfcf9bf64178b1e
Oshani <oshanis@gmail.com>
2014-08-01 11:55:09 -0400

    cc licenses

--- public/js/audit.js
+++ public/js/audit.js
@@ -13,41 +13,53 @@ function audit(resource){
   		else {
   			for (var i=0; i< data.length; i++){
   				
-  				var time_p = $(document.createElement('p'));
-  				time_p.append(data[i].time);
-				$( ".modal-body" ).append(time_p);
+  				if (data[i].name != "access"){ //Otherwise can get too big too fast
 
-  				
-  				var user_span = document.createElement('span');
-				user_span.appendChild(document.createTextNode(data[i].name));
-  				user_span.appendChild(document.createTextNode(" by "));
-				$( ".modal-body" ).append(user_span);
+  					var time_p = $(document.createElement('p'));
+  					time_p.append(data[i].time);
+					$( ".modal-body" ).append(time_p);
+
+				  	var user_span = document.createElement('span');
+					user_span.appendChild(document.createTextNode(data[i].name));
+	  				user_span.appendChild(document.createTextNode(" by "));
+					$( ".modal-body" ).append(user_span);
+
+					var user_a = document.createElement('a');
+					//user_a.href = JSON.stringify(data[i].details.user);
+					user_a.href = data[i].details.user;
+					user_a.target = "_blank";
+					user_a.appendChild(document.createTextNode(data[i].details.name + " "));
+
+				 	$( ".modal-body" ).append(user_a);
 
 
-				var user_a = document.createElement('a');
-				//user_a.href = JSON.stringify(data[i].details.user);
-				user_a.href = data[i].details.user;
-				user_a.target = "_blank";
-				user_a.appendChild(document.createTextNode(data[i].details.name + " "));
+				 	$( ".modal-body" ).append(document.createTextNode(" as "));
 
+					var derivative_a = document.createElement('a');
+					//user_a.href = JSON.stringify(data[i].details.user);
+					derivative_a.href = data[i].derivative;
+					derivative_a.target = "_blank";
+					derivative_a.appendChild(document.createTextNode(data[i].derivative + " "));
 
+					$( ".modal-body" ).append(derivative_a);
+				 	
+					var question_a = document.createElement('a');
+					question_a.href = "mailto:"+data[i].details.email;
+					question_a.target = "_blank";
 
-			 	$( ".modal-body" ).append(user_a);
+					var question_button = document.createElement('button');
+					question_button.class = "btn btn-default";
+					question_button.appendChild(document.createTextNode("?"));
 
-			 	
-				var question_a = document.createElement('a');
-				question_a.href = "mailto:"+data[i].details.email;
-				question_a.target = "_blank";
+					question_a.appendChild(question_button);
 
-				var question_button = document.createElement('button');
-				question_button.class = "btn btn-default";
-				question_button.appendChild(document.createTextNode("?"));
+					$( ".modal-body" ).append(question_a);
 
-				question_a.appendChild(question_button);
+					$( ".modal-body" ).append(document.createElement('hr'));
 
-				$( ".modal-body" ).append(question_a);
+  				}
 
-				$( ".modal-body" ).append(document.createElement('hr'));
+				
 
   			}
   			
